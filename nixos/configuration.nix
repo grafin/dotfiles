6f6357b211b34ccf9b8e8879ffed8982a8fc1a21
Boris Stepanenko <Boris.Stepanenko@gmail.com>
2021-03-08 13:04:46 +0300

    nixos/configuration: updated network configuration

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -36,17 +36,18 @@
 
   # Network
   networking = {
-    hostName = "nixos";
+    hostName = "mainhost";
     useDHCP = false;
 
     # Interfaces
-    interfaces.enp0s3.ipv4.addresses = [
-      {
-        address = "169.254.1.5";
-        prefixLength = 16;
-      }
-    ];
-    defaultGateway = "169.254.1.1";
+    # interfaces.enp0s3.ipv4.addresses = [
+    #   {
+    #     address = "169.254.1.5";
+    #     prefixLength = 16;
+    #   }
+    # ];
+    # defaultGateway = "169.254.1.1";
+    interfaces.enp3s0.useDHCP = true;
     nameservers = [ "1.1.1.1" "1.0.0.1" ];
 
     # Proxy
