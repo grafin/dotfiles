ae87256d373503b66e98429f74daed1ebd3e7df8
Boris Stepanenko <Boris.Stepanenko@gmail.com>
2021-03-08 17:42:20 +0300

    nixos/packages: added GUI packages

--- nixos/packages/gui/default.nix
+++ nixos/packages/gui/default.nix
@@ -2,10 +2,32 @@
 
 {
   environment.systemPackages = with pkgs; [
+    audacity
+    chromium
+    dbeaver
+    deadbeef
+    discord
+    dropbox
     firefox
+    geogebra
+    gimp
+    jitsi
     keepassxc
-    sublime3
+    libreoffice
+    mellowplayer
+    qucs
+    remmina
+    shotcut
+    skype
+    sourcetrail
     sublime-merge
+    sublime3
     tdesktop
+    thunderbird
+    transmission
+    vlc
+    wireshark
+    xournalpp
+    zoom-us
   ];
 }
