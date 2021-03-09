68d4c5ec213a32ee60cc78732b41909dfe6dd204
Boris Stepanenko <Boris.Stepanenko@gmail.com>
2021-03-09 13:01:33 +0300

    network: added veth configuration

--- nixos/network.nix
+++ nixos/network.nix
@@ -1,26 +1,56 @@
 { ... }:
 
 {
-  # Network
   networking = {
     hostName = "mainhost";
     useDHCP = false;
 
-    # Interfaces
-    # interfaces.enp0s3.ipv4.addresses = [
-    #   {
-    #     address = "169.254.1.5";
-    #     prefixLength = 16;
-    #   }
-    # ];
-    # defaultGateway = "169.254.1.1";
-    interfaces.enp3s0.useDHCP = true;
-    nameservers = [ "1.1.1.1" "1.0.0.1" ];
+    bridges.br0.interfaces = [ "enp3s0" "veth_br0_0" ];
+    interfaces.br0.useDHCP = true;
 
-    # Proxy
-    # proxy = {
-    #   default = "http://user:password@proxy:port/";
-    #   noProxy = "127.0.0.1,localhost,internal.domain";
-    # }
+    interfaces.veth_ll_0.ipv4.addresses = [
+      {
+        address = "169.254.1.1";
+        prefixLength = 16;
+      }
+    ];
+
+    interfaces.veth_vipnet_0.ipv4 = {
+      addresses = [
+        {
+          address = "172.17.0.2";
+          prefixLength = 24;
+        }
+      ];
+      routes = [
+        {
+          address = "11.0.0.0";
+          prefixLength = 8;
+          via = "172.17.0.1";
+        }
+        {
+          address = "10.0.0.0";
+          prefixLength = 16;
+          via = "172.17.0.1";
+        }
+        {
+          address = "172.31.0.0";
+          prefixLength = 16;
+          via = "172.17.0.1";
+        }
+        {
+          address = "192.168.40.0";
+          prefixLength = 24;
+          via = "172.17.0.1";
+        }
+        {
+          address = "192.168.100.0";
+          prefixLength = 24;
+          via = "172.17.0.1";
+        }
+      ];
+    };
+
+    nameservers = [ "172.17.0.1" ];
   };
 }
