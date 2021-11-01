56f465be352ff46ece52dc35cf978d0f7822ad07
Boris Stepanenko <Boris.Stepanenko@gmail.com>
2021-11-01 11:11:58 +0300

    R: use same packages list for R and rstudio

--- nixos/packages/gui/R/R.nix
+++ nixos/packages/gui/R/R.nix
@@ -1,17 +1,26 @@
 { pkgs, ... }:
 
-{
+let
+  rPackages = with pkgs.rPackages; [
+    FinancialMath
+    PerformanceAnalytics
+    R_utils
+    data_table
+    ggplot2
+    git2r
+    gsheet
+    htmlwidgets
+    plotly
+    readODS
+    rmarkdown
+  ];
+in {
   environment.systemPackages = with pkgs; [
-    (rstudioWrapper.override{
-      packages = with rPackages; [
-        FinancialMath
-        PerformanceAnalytics
-        data_table
-        ggplot2
-        gsheet
-        readODS
-        rmarkdown
-      ];
+    (rstudioWrapper.override {
+      packages = rPackages;
+    })
+    (rWrapper.override {
+      packages = rPackages;
     })
   ];
 }
