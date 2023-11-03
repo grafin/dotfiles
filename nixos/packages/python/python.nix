b71b162adc73d7de23abc42d6df6fe4911bff1eb
Borislav Demidov <Boris.Stepanenko@gmail.com>
2023-11-03 17:10:51 +0300

    python: use latest sympy

--- nixos/packages/python/python.nix
+++ nixos/packages/python/python.nix
@@ -1,33 +1,13 @@
 { pkgs, ... }:
 
 let
-  sympy_1_5_1 = pkgs.python3.pkgs.buildPythonPackage rec {
-    pname = "sympy";
-    version = "1.5.1";
-
-    src = pkgs.python3.pkgs.fetchPypi {
-      inherit pname version;
-      sha256 = "0zjfbxlkazzh9z22gf62azrkipb2xw7mpzjz3wl1az9893bh2yfp";
-    };
-
-    propagatedBuildInputs = with pkgs; [
-      python3Packages.mpmath
-    ];
-
-    doCheck = false;
-
-    meta = {
-      homepage = "https://www.sympy.org/en/index.html";
-      description = "Python library for symbolic mathematics";
-    };
-  };
 
   my-python-packages = python-packages: with python-packages; [
     flake8
     jedi
     pygments
     pylint
-    sympy_1_5_1
+    sympy
   ];
 
   my-python = pkgs.python3.withPackages my-python-packages;
