222572a3676b01a449daeba49699526d7d793d55
Borislav Demidov <Boris.Stepanenko@gmail.com>
2023-11-03 17:07:42 +0300

    vim: add jqx

--- nixos/packages/vim/vim.nix
+++ nixos/packages/vim/vim.nix
@@ -38,6 +38,7 @@ in {
             lightline-vim
             nerdtree
             nerdtree-git-plugin
+            nvim-jqx
             omnisharp-vim
             papercolor-theme
             typescript-vim
