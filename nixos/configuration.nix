a5f620ce9146ad9005be244b2e72b614b996ee34
Borislav Demidov <Boris.Stepanenko@gmail.com>
2026-02-19 00:41:10 +0300

    nixos: use gtk2 pinentry

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -75,8 +75,8 @@
   programs.gnupg.agent = {
     enable = true;
     enableSSHSupport = true;
-    #pinentryPackage = pkgs.pinentry-gtk2;
-    pinentryPackage = pkgs.pinentry-curses;
+    pinentryPackage = pkgs.pinentry-gtk2;
+    #pinentryPackage = pkgs.pinentry-curses;
   };
 
   # Podman
